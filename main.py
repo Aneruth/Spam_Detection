593d7455d1aa25c9aa78ccfbc3996ccdbbf09826
Aneruth Mohanasundaram <ane1998@gmail.com>
2022-01-16 12:19:09 +0100

    refactoring of the code and PCA Neural Network graph correction

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys,os
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
 sys.path.insert(0,os.path.join('Code/Models'))
 
-import SupportVectorMachine,NaiveBayes,RandomForest,NeuralNetwork
+import SupportVectorMachine,NaiveBayes,RandomForest,NeuralNetwork,lstm
 
 data = 'YoutubeComplete.csv'
 
@@ -13,3 +15,7 @@ rf = RandomForest.randomForest(data).metrics()
 nb = NaiveBayes.NaiveBayes(data).naiveBayes()
 
 nerural = NeuralNetwork.NeuralNet(data).metrices()
+
+rnn = lstm.LSTMImplement(data,28,4,1).run()
+
+nerural = NeuralNetwork.NeuralNetPca(data).nnPCA()
