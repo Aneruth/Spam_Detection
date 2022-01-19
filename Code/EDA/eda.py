b63df680dde1ac5f500b2a62bfccb9546e10698b
Aneruth Mohanasundaram <ane1998@gmail.com>
2022-01-19 22:17:00 +0100

    EDA word cloud addition

--- Code/EDA/eda.py
+++ Code/EDA/eda.py
@@ -1,5 +1,7 @@
-import sys,os
-sys.path.insert(0,os.path.join('Code/Preprocess'))
+import sys
+import os
+sys.path.insert(0, os.path.join('Code/Preprocess'))
+
 
 class ExploratoryDataAnalysis:
     """A class for exploratory data analysis of our dataset where we summarize the main characteristics of our dataset
@@ -15,10 +17,11 @@ class ExploratoryDataAnalysis:
         import matplotlib.pyplot as plt
         from collections import Counter
         import pandas as pd
-        
+        from wordcloud import WordCloud
+
         data = Preprocess()
         data = data.preprocessMethod('YoutubeComplete.csv')
-        data.replace({0.0:'Ham',1.0:'Spam'},inplace=True)
+        data.replace({0.0: 'Ham', 1.0: 'Spam'}, inplace=True)
         data['length'] = data.CONTENT.apply(len)
 
         immedDir = Path(__file__).parent.parent
@@ -27,50 +30,55 @@ class ExploratoryDataAnalysis:
         # Check if the output folder path present if not create it
         if not os.path.exists(os.path.join(immedDir, "Images")):
             os.mkdir(os.path.join(immedDir, "Images"))
-        
+
         if not os.path.exists(os.path.join(immedDir, "Images/Eda")):
             os.mkdir(os.path.join(immedDir, "Images/Eda"))
 
-        fig = data.hist(column='length', by='CLASS', bins=50,figsize=(11,5))
-        plt.savefig( os.path.join( path_to_save, "Content Length Distribution"+".png" ))
+        fig = data.hist(column='length', by='CLASS', bins=50, figsize=(11, 5))
+        plt.savefig(os.path.join(
+            path_to_save, "Content Length Distribution"+".png"))
         plt.show(block=False)
         plt.pause(3)
         plt.close()
 
-        f,ax=plt.subplots(1,2, figsize=(12,4))
-        data.CLASS.value_counts().plot.pie(explode=[0,0.12],autopct='%1.3f%%',ax=ax[0])
+        f, ax = plt.subplots(1, 2, figsize=(12, 4))
+        data.CLASS.value_counts().plot.pie(
+            explode=[0, 0.12], autopct='%1.3f%%', ax=ax[0])
         ax[0].set_title('Class Ratio')
-        sns.countplot('CLASS',data=data)
+        sns.countplot('CLASS', data=data)
         ax[1].set_title('Class Distribution')
-        plt.savefig( os.path.join( path_to_save, "Class Distribution and Ratio"+".png" ))
+        plt.savefig(os.path.join(
+            path_to_save, "Class Distribution and Ratio"+".png"))
         plt.show(block=False)
         plt.pause(3)
         plt.close()
 
-        hamWords = list(data.loc[data.CLASS == 'Ham','CONTENT'])
-        hamWordFreq = pd.DataFrame(Counter(" ".join(hamWords).split()).most_common(50), columns = ['word', 'frequency'])
-        fig, ax = plt.subplots(figsize = (15, 5))
-        sns.barplot(x = 'word', y = 'frequency', data = hamWordFreq, ax = ax)
-        plt.xticks(rotation = '90')
-        plt.title("50 most common ham words")
-        plt.savefig( os.path.join( path_to_save, "50 most common ham words"+".png" ))
+        hamWords = list(data.loc[data.CLASS == 'Ham', 'CONTENT'])
+        text_wordcloud_ham = " ".join(x for x in hamWords)
+        wordcloud = WordCloud(
+            background_color="white").generate(text_wordcloud_ham)
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.title("Frequently Used Ham Words")
+        plt.axis("off")
+        plt.savefig(os.path.join(
+            path_to_save, "Frequently Used Ham Words"+".png"))
         plt.show(block=False)
         plt.pause(3)
         plt.close()
 
-        spamWords = list(data.loc[data.CLASS == 'Spam','CONTENT'])
-        spamWordFreq = pd.DataFrame(Counter(" ".join(spamWords).split()).most_common(50), columns = ['word', 'frequency'])
-        fig, ax = plt.subplots(figsize = (15, 5))
-        sns.barplot(x = 'word', y = 'frequency', data = spamWordFreq, ax = ax)
-        plt.xticks(rotation = '90')
-        plt.title("50 most common spam words")
-        plt.savefig( os.path.join( path_to_save, "50 most common spam words"+".png" ))
+        spamWords = list(data.loc[data.CLASS == 'Spam', 'CONTENT'])
+        text_wordcloud_spam = " ".join(x for x in spamWords)
+        wordcloud = WordCloud(
+            background_color="white").generate(text_wordcloud_spam)
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.title("Frequently Used Spam Words")
+        plt.axis("off")
+        plt.savefig(os.path.join(
+            path_to_save, "Frequently Used Spam Words"+".png"))
         plt.show(block=False)
         plt.pause(3)
         plt.close()
 
 
-
-
-# if __name__ == '__main__':
-#     ExploratoryDataAnalysis.visualise()
+if __name__ == '__main__':
+    ExploratoryDataAnalysis.visualise()
