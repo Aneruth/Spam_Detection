65b98e0fff97f1edb75a7df193b73a8c132de355
Aneruth Mohanasundaram <ane1998@gmail.com>
2023-03-23 12:00:01 +0100

    Stage 1 completed, Create a docker image starts

--- spam_classifier/Models/NaiveBayes.py
+++ spam_classifier/Models/NaiveBayes.py
@@ -1,128 +1,45 @@
-from posixpath import abspath
-import sys,os
-from pathlib import Path
-sys.path.insert(0,os.path.join('spam_classifier/Preprocess'))
-from DataPreparation import dataPrepare
-
 import warnings
+from sklearn.naive_bayes import GaussianNB
+from spam_classifier.pipelines import train_pipeline
+from sklearn.metrics import *
 warnings.filterwarnings("ignore")
 
-#Import Gaussian Naive Bayes model
-from sklearn.naive_bayes import GaussianNB
 
 class NaiveBayes:
 
-    def __init__(self,path) -> None:
-        self.path = path
-    
-    def nb(self):
+    def __init__(self):
+        self.y_hat = None
+        self.y_test = None
+
+    def run_model(self):
         """The Bayes' Theorem is used to preprocess for classification techniques known as 
         Naive Bayes classifiers. It is a family of algorithms that share a similar idea, 
         namely that each pair of features being classified is independent of the others.
-
-        Returns:
-            [list]: returns label test data and prediction values
         """
-        fetchData = dataPrepare()
-        self.X_train, self.X_test, self.y_train, self.y_test = fetchData.Vectorizer(self.path)
-
-        #Create a Gaussian Classifier
+        X_train, X_test, y_train, self.y_test = train_pipeline.vectorize()
         nb = GaussianNB()
+        nb.fit(X_train.toarray(), y_train)
+        self.y_hat = nb.predict(X_test.toarray())
 
-        # Train the model using the training sets
-        # https://stackoverflow.com/questions/30502284/a-sparse-matrix-was-passed-but-dense-data-is-required-use-x-toarray-to-conve/37248794
-        nb.fit(self.X_train.todense(),self.y_train)
-
-        nb_prediction = nb.predict(self.X_test.todense())
-        
-        # nb_accuracy_score = round(accuracy_score(y_test, nb_prediction)*100,2)
-        # print(f"Accuracy score for Naive bayes is: {nb_accuracy_score}%")
-        return self.y_test,nb_prediction
-    
-    def naiveBayes(self):
-        """A function which calculates all the metrices and plots the graph.
+    def get_score(self) -> dict:
+        """Get the accuracy score of the model
 
         Returns:
-            float: Returns the predcited score value
+            dict: returns dictionary of accuracy, precision, recall and f1 score
         """
-        import matplotlib.pyplot as plt
+        accuracy = accuracy_score(self.y_test, self.y_hat)
+        precision = precision_score(self.y_test, self.y_hat)
+        recall = recall_score(self.y_test, self.y_hat)
+        f1 = f1_score(self.y_test, self.y_hat)
+        return {
+            "accuracy": accuracy,
+            "precision": precision,
+            "recall": recall,
+            "f1": f1
+        }
 
-        self.y_test,nb_prediction = self.nb()
 
-        from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
-        print(f'The confusion matrix for Support Vector Machine before hyper parameter tunning is \n{confusion_matrix(self.y_test,nb_prediction)}')
-
-        print(f'\n Classification report for Support Vector Machine before hyper parameter tunning is:\n{classification_report(self.y_test,nb_prediction)}')
-
-        acc_score_before_hyper = round(accuracy_score(self.y_test,nb_prediction)*100,2)
-        print(f'Accuracy Score before hyper parameter tunning is: {acc_score_before_hyper}%')
-
-        '''
-        Hpyperparameter tuning the model with the help of grid search
-
-        Performing grid search which is a type of cross validation for Support Vector Machine
-        - C is a hypermeter which is set before the training model and used to control error.
-        - Gamma is also a hypermeter which is set before the training model and used to give curvature weight of the decision boundary.
-        - More gamma more the curvature and less gamma less curvature.
-        '''
-        # Initialise the gird search variable 
-        import numpy as np
-        param_grid_nb = {'var_smoothing': np.logspace(0,-9, num=100)}
-
-        '''
-        Important parameter for grid search 
-            - verbose --> get the quality of the data more the verbose more the quality
-            - scoring --> a parameter where we define what we need to infer from the dataset
-        '''
-        from sklearn.model_selection import GridSearchCV
-        # feed the search variable to the grid search 
-        grid = GridSearchCV(estimator=GaussianNB(), param_grid=param_grid_nb, verbose=1, cv=10, n_jobs=-1)
-
-        # Grid Search fixable
-        grid.fit(self.X_train.todense(),self.y_train)
-
-        # to get the best parameter fit from the grid
-        print('The best parameter is:',grid.best_params_)
-        
-        # to get the predictions from the grid
-        grid_predict = grid.predict(self.X_test.todense())
-
-        # To print the classification report and confusion matrix for the SVM
-        print('Report for Suport Vector Machine')
-        # To print the confusion matrix
-        print('The confusion matrix after grid search is:' + '\n \n',confusion_matrix(self.y_test,grid_predict))
-
-        # To print the classification report
-        print('\nThe classification report after grid search is:' + f'\n{classification_report(self.y_test,grid_predict)}')
-       
-        immedDir = Path(__file__).parent.parent
-        # parentDir = os.path.dirname(abspath(immedDir))
-       
-        # to get the best score from the grid
-        print('The best score is:',((grid.best_score_)*100).round(2),'%')
-        print()
-        s = [acc_score_before_hyper,((grid.best_score_)*100).round(2)]
-        n = ['Before Hyperparameter','After Hyperparameter']
-        plt.figure(figsize=(12,6))
-        plt.title('Graph to compare accuracy score before and after hyperparameter tunning')
-        plt.xlabel('Naive Bayes Algorithm')
-        plt.ylabel('Accuracy Score')
-        plt.bar(['Before Hyperparameter','After Hyperparameter'],[acc_score_before_hyper,((grid.best_score_)*100).round(2)])
-        for i in range(len(s)):
-            plt.annotate(str(s[i]), xy=(n[i],s[i]), ha='center', va='bottom')
-        
-        path_to_save = os.path.join(immedDir, "Images/NaiveBayes")
-        
-        # Check if the output folder path present if not create it
-        if not os.path.exists(os.path.join(immedDir, "Images")):
-            os.mkdir(os.path.join(immedDir, "Images"))
-        
-        if not os.path.exists(os.path.join(immedDir, "Images/NaiveBayes")):
-            os.mkdir(os.path.join(immedDir, "Images/NaiveBayes"))
-        
-        plt.savefig( os.path.join( path_to_save, "NaiveBayesAccPlotfor"+self.path.split("/")[-1].split(".")[0]+".png" ))
-        
-        plt.show(block=False)
-        plt.pause(3)
-        plt.close()
-        return ((grid.best_score_)*100).round(2)
+if __name__ == "__main__":
+    nb = NaiveBayes()
+    nb.run_model()
+    print(nb.get_score())
--- spam_classifier/pipelines/train_pipeline.py
+++ spam_classifier/pipelines/train_pipeline.py
@@ -1,7 +1,6 @@
-import numpy as np
-from config.core import config
-from pipeline import CreatePipeline
-from Preprocess.preprocess import Parser
+from spam_classifier.config.core import config
+from spam_classifier.pipelines.pipeline import CreatePipeline
+from spam_classifier.Preprocess.preprocess import Parser
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -34,22 +33,21 @@ def run_training() -> tuple:
     return X_train, X_test, y_train, y_test
 
 
-def vectorize():
-    X_tr, X_ts, y_tr, y_ts = run_training()
+def vectorize() -> tuple:
+    """ Vectorize the text data using tfidf method
+    @return: A tuple of transformed X_train, X_test and y_train, y_test
+    """
+    X_train, X_test, y_train, y_test = run_training()
 
     # Since we are using the text as input, we need to convert it to a sparse matrix
     # We are using the tfidf method to convert our text to a sparse matrix
     tfidf = TfidfVectorizer(encoding="latin-1", strip_accents="unicode", stop_words="english")
 
     # Fit and transform the training data
-    # For strange reason I need to convert the list to a string
-    feat = tfidf.fit_transform(X_tr['CONTENT'].tolist())
+    # For strange reason I need to convert the list to a string or a numpy array
+    # NOTE: TFIDF is not working with pandas dataframe
+    X_tr_transform = tfidf.fit_transform(X_train['CONTENT'])
 
-    print(X_tr.shape, feat.shape)
+    X_ts_transform = tfidf.transform(X_test['CONTENT'])
 
-    # TODO: Use this inside the model and containerize it using docker
-
-
-if __name__ == "__main__":
-
-    vectorize()
+    return X_tr_transform, X_ts_transform, y_train, y_test
